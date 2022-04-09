720d40e6bee2b4f21d394302520fc32f18c7b7fc
Snare <sh0n556@bk.ru>
2022-04-09 23:15:49 +0300

    created kinda shitty Tree class, but it will help a little

--- include/Tree.h
+++ include/Tree.h
@@ -1,7 +1,10 @@
 #ifndef __TREE
 #define __TREE
 
+#include <memory>
+
 #include "../Debug.h"
+
 
 namespace LEti {
 
@@ -15,23 +18,27 @@ namespace LEti {
 			Node* child[_childs_per_node] = { nullptr };
 			Node* parent = nullptr;
 
-			Node(Stored_Type* _new_data) : data(_new_data) { ASSERT(_new_data == nullptr); }
+			Node(const Stored_Type& _new_data) : data(new Stored_Type(_new_data)) {  }
+			Node(Stored_Type&& _new_data) : data(new Stored_Type(_new_data)) {  }
 			~Node() { delete data; }
 
-			void insert_after(Stored_Type* _new_data, unsigned int _child_index);
+			void insert_after(const Stored_Type& _new_data, unsigned int _child_index);
+			void insert_after(Stored_Type&& _new_data, unsigned int _child_index);
+
+			static void clear_branch(Node* _node);
 		};
 
 		Node* head = nullptr;
 
 	public:
-		friend class Iterator;
+		~Tree();
+
+	public:
 		class Iterator
 		{
 		private:
 			Node** head;
 			Node* node = nullptr;
-
-		private:
 
 		public:
 			Iterator() {  }
@@ -40,27 +47,49 @@ namespace LEti {
 			Iterator(const Iterator& _other) : node(_other.node) {  }
 			void operator=(const Iterator& _other) { node = _other.node; }
 
+		private:
+			unsigned int get_child_index();
+
 		public:
+			bool valid() const;
+
 			Stored_Type& operator*() { return *(node->data); }
-			
+
 			bool operator==(const Iterator& _other) { return node == _other.node; }
 			bool operator!=(const Iterator& _other) { return !(*this == _other); }
 
-			void insert_after(Stored_Type* _new_data, unsigned int _child_index);
+			void operator++();
+			void operator--();
+
+			void insert_after(const Stored_Type& _new_data, unsigned int _child_index);
+			void insert_after(Stored_Type&& _new_data, unsigned int _child_index);
+
+			void delete_branch();
 
 			void ascend();
 			void descend(unsigned int _child_index);
+
+			bool begin() const;
+			bool end() const;
 		};
 
-		Iterator begin() { return Iterator(head); }
-		Iterator end() { return Iterator(); }
+		Iterator create_iterator() { return Iterator(head); }
+
+	public:
 
 	};
 
+	template<typename Stored_Type, unsigned int _cpn>
+	Tree<Stored_Type, _cpn>::~Tree()
+	{
+		Node::clear_branch(head);
+	}
 
+
+	//NODE IMPLEMENTATION
 
 	template<typename Stored_Type, unsigned int _cpn>
-	void Tree<Stored_Type, _cpn>::Node::insert_after(Stored_Type* _new_data, unsigned int _child_index)
+	void Tree<Stored_Type, _cpn>::Node::insert_after(const Stored_Type& _new_data, unsigned int _child_index)
 	{
 		ASSERT(_child_index >= _cpn);
 		Node* pnext = child[_child_index];
@@ -73,10 +102,55 @@ namespace LEti {
 		}
 	}
 
+	template<typename Stored_Type, unsigned int _cpn>
+	void Tree<Stored_Type, _cpn>::Node::insert_after(Stored_Type&& _new_data, unsigned int _child_index)
+	{
+		ASSERT(_child_index >= _cpn);
+		Node* pnext = child[_child_index];
+		child[_child_index] = new Node(std::move(_new_data));
+		child[_child_index]->parent = this;
+		if (pnext)
+		{
+			child[_child_index]->child[0] = pnext;
+			pnext->parent = child[_child_index];
+		}
+	}
+
+	template<typename Stored_Type, unsigned int _cpn>
+	void Tree<Stored_Type, _cpn>::Node::clear_branch(Node* _node)
+	{
+		if (!_node) return;
+
+		for (unsigned int i = 0; i < _cpn; ++i)
+		{
+			clear_branch(_node->child[i]);
+			//delete _node->child[i];
+		}
+		delete _node;
+	}
+
+
+	//ITERATOR IMPLEMENTATION
+
+	template<typename Stored_Type, unsigned int _cpn>
+	unsigned int Tree<Stored_Type, _cpn>::Iterator::get_child_index()
+	{
+		ASSERT(!valid());
+		if (!node->parent) return _cpn;
+		for (unsigned int i = 0; i < _cpn; ++i)
+			if (node->parent->child[i] == node) return i;
+	}
 
 
 	template<typename Stored_Type, unsigned int _cpn>
-	void Tree<Stored_Type, _cpn>::Iterator::insert_after(Stored_Type* _new_data, unsigned int _child_index)
+	bool Tree<Stored_Type, _cpn>::Iterator::valid() const
+	{
+		return *head != nullptr && node != nullptr;
+	}
+
+
+	template<typename Stored_Type, unsigned int _cpn>
+	void Tree<Stored_Type, _cpn>::Iterator::insert_after(const Stored_Type& _new_data, unsigned int _child_index)
 	{
 		if (!*head)
 		{
@@ -85,6 +159,42 @@ namespace LEti {
 		}
 		else node->insert_after(_new_data, _child_index);
 	}
+
+	template<typename Stored_Type, unsigned int _cpn>
+	void Tree<Stored_Type, _cpn>::Iterator::insert_after(Stored_Type&& _new_data, unsigned int _child_index)
+	{
+		if (!*head)
+		{
+			*head = new Node(std::move(_new_data));
+			node = *head;
+		}
+		else
+		{
+			node->insert_after(std::move(_new_data), _child_index);
+		}
+	}
+
+	template<typename Stored_Type, unsigned int _cpn>
+	void Tree<Stored_Type, _cpn>::Iterator::delete_branch()
+	{
+		ASSERT(!*head || !node);
+
+		unsigned int ind = get_child_index();
+
+		if (ind == _cpn)
+		{
+			Node::clear_branch(*head);
+			*head = nullptr;
+			node = nullptr;
+		}
+		else
+		{
+			Node* pptr = node->parent;
+			Node::clear_branch(pptr->child[ind]);
+			pptr->child[ind] = nullptr;
+		}
+	}
+
 
 	template<typename Stored_Type, unsigned int _cpn>
 	void Tree<Stored_Type, _cpn>::Iterator::ascend()
@@ -100,7 +210,125 @@ namespace LEti {
 		ASSERT(!head);
 		ASSERT(_ci >= _cpn);
 		ASSERT(!node);
+		ASSERT(node->child[_ci] == nullptr);
 		node = node->child[_ci];
+	}
+
+
+	template<typename Stored_Type, unsigned int _cpn>
+	void Tree<Stored_Type, _cpn>::Iterator::operator++()
+	{
+		ASSERT(!*head);
+		ASSERT(!node);
+
+		for (unsigned int i = 0; i < _cpn; ++i)
+		{
+			if (node->child[i])
+			{
+				node = node->child[i];
+				return;
+			}
+		}
+
+		Node* tptr = node;
+		while (tptr)
+		{
+			Node* tptr_p = tptr->parent;
+			if (!tptr_p) return;
+
+			unsigned int i = 1;
+			for (; i < _cpn; ++i)
+				if (tptr_p->child[i - 1] == tptr) break;
+			for (; i < _cpn; ++i)
+			{
+				if (tptr_p->child[i])
+				{
+					node = tptr_p->child[i];
+					return;
+				}
+			}
+
+			tptr = tptr_p;
+		}
+	}
+
+	template<typename Stored_Type, unsigned int _cpn>
+	void Tree<Stored_Type, _cpn>::Iterator::operator--()
+	{
+		ASSERT(!*head);
+		ASSERT(!node);
+
+		Node* tptr = node;
+		Node* tptr_p = tptr->parent;
+		if (!tptr_p) return;
+
+
+		for (int i = get_child_index() - 1; i >= 0; --i)
+		{
+			if (tptr_p->child[i])
+			{
+				tptr = tptr_p->child[i];
+
+				while (true)
+				{
+					int c = _cpn - 1;
+					for (; c >= 0; --c)
+					{
+						if (tptr->child[c])
+						{
+							tptr = tptr->child[c];
+							break;
+						}
+					}
+					if (c == -1)
+					{
+						node = tptr;
+						return;
+					}
+				}
+			}
+		}
+
+		node = tptr_p;
+	}
+
+
+	template<typename Stored_Type, unsigned int _cpn>
+	bool Tree<Stored_Type, _cpn>::Iterator::begin() const
+	{
+		ASSERT(!valid());
+		return node->parent == nullptr;
+	}
+
+	template<typename Stored_Type, unsigned int _cpn>
+	bool Tree<Stored_Type, _cpn>::Iterator::end() const
+	{
+		ASSERT(!valid());
+
+		if (node->parent == nullptr || node->parent->child[_cpn - 1] == node)
+		{
+			for (unsigned int i = 0; i < _cpn; ++i)
+				if (node->child[i]) return false;
+		}
+
+		for (unsigned int i = 0; i < _cpn; ++i)
+			if (node->child[i]) return false;
+
+		Node* tptr = node;
+		while (tptr->parent)
+		{
+			if (tptr->parent->child[_cpn - 1] != tptr)
+			{
+				unsigned int i = 1;
+				for (; i < _cpn; ++i)
+					if (tptr->parent->child[i - 1] == tptr) break;
+				for (; i < _cpn; ++i)
+					if (tptr->parent->child[i]) return false;
+			}
+			tptr = tptr->parent;
+		}
+
+		return true;
 	}
 
 
