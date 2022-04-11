7508a486d7bdcf0266ee3f17501f1b9fe5e1dd3f
a.hiryanov <a.hiryanov@octaviangameart.ru>
2022-04-11 18:06:39 +0300

    added EIPF (extremely ineficient path-finder) class

---
+++ include/EIPF.h
@@ -0,0 +1,73 @@
+#ifndef __A_STAR
+#define __A_STAR
+
+#include <cmath>
+#include <vector>
+
+#include "../Debug.h"
+
+#include "Tree.h"
+
+
+namespace LEti {
+
+    class EIPF  //Extremely Ineficient Path Finder
+    {
+    private:
+        enum class Direction { up, down, left, right };
+        void rotate_cw(Direction& _d) const;
+
+    private:
+        struct intp
+        {
+            int x = 0, y = 0;
+
+            intp(int _x, int _y) : x(_x), y(_y) {}
+
+            bool operator==(const intp& _other) const { return x == _other.x && y == _other.y; }
+            bool operator!=(const intp& _other) const { return !(*this == _other); }
+
+            void move(Direction _d);
+            intp after_movement(Direction _d) const;
+        };
+
+    private:
+        bool** m_occupied = nullptr;
+        unsigned int m_size_x = 0, m_size_y = 0;
+
+    private:
+        bool is_step_unique(const intp& _step) const;
+        unsigned int get_distance(const intp& _first, const intp& _second) const;
+        bool out_of_bounds(const intp& _ind) const;
+        bool at(const intp& _ind) const;
+
+        bool can_move_here(const EIPF::intp &_where) const;
+
+        std::pair<LEti::Tree<EIPF::intp, 3>::Iterator, EIPF::intp> find_closest_pos(const intp& _dest) const;
+
+    private:
+        LEti::Tree<intp, 3> m_tree;
+        LEti::Tree<intp, 3>::Const_Iterator m_path_end;
+        bool m_path_found = false;
+        unsigned int m_steps_count = 0;
+        intp m_current_pos = {-1, -1}, m_destination = {-1, -1};
+
+    public:
+        EIPF();
+        ~EIPF();
+
+    public:
+        void set_field_size(unsigned int _x, unsigned int _y);
+        void set_destination(unsigned int _x, unsigned int _y);
+        void set_start_pos(unsigned int _x, unsigned int _y);
+        bool& field_element(unsigned int _x, unsigned int _y);
+
+    public:
+        void build_path();
+        std::pair<int, int> get_next_step() const;
+        std::vector<std::pair<int, int>> get_full_path() const;
+
+    };
+}
+
+#endif // __A_STAR
