1fb5c538ed37af063a60157d3a07c62412e2ec37
a.hiryanov <a.hiryanov@octaviangameart.ru>
2023-02-27 14:58:57 +0300

    added alignment to text field... need to seriously think about refactoring this mess

--- include/Object_System/Text_Field.h
+++ include/Object_System/Text_Field.h
@@ -29,15 +29,32 @@ namespace LEti
 
 	class Text_Field : public Object_2D
 	{
-	private:
-        std::string sequence;
-		std::pair<const float*, unsigned int> text_tex_coords;
+    public:
+        enum class Horizontal_Alignment
+        {
+            left,
+            right,
+            center
+        };
+        enum class Vertical_Alignment
+        {
+            top,
+            bottom,
+            center
+        };
 
-		std::map<unsigned char, const float*> sequence_map;
+	private:
+        std::string m_sequence;
+        std::pair<const float*, unsigned int> m_text_tex_coords;
+
+        std::map<unsigned char, const float*> m_sequence_map;
+
+        Horizontal_Alignment m_horizontal_alignment = Horizontal_Alignment::left;
+        Vertical_Alignment m_vertical_alignment = Vertical_Alignment::bottom;
 
 	private:
-		float width = 0.0f, height = 0.0f, actual_symbol_width = 0.0f;
-        std::string text;
+        float m_width = 0.0f, m_height = 0.0f, m_actual_symbol_width = 0.0f;
+        std::string m_text;
 
 	public:
 		Text_Field();
@@ -45,9 +62,14 @@ namespace LEti
 	public:
 		void init(const LV::Variable_Base& _stub) override;
 
+    public:
+        inline void set_horizontal_alignment(Horizontal_Alignment _ha) { m_horizontal_alignment = _ha; }
+        inline void set_vertical_alignment(Vertical_Alignment _va) { m_vertical_alignment = _va; }
+
 	public:
         void set_text(const std::string& _text);
-        const std::string& get_text() const;
+
+        inline const std::string& get_text() const { return m_text; }
 
 	};
 
--- source/Object_System/Text_Field.cpp
+++ source/Object_System/Text_Field.cpp
@@ -34,57 +34,70 @@ void Text_Field::init(const LV::Variable_Base &_stub)
 	remove_draw_module();
 	remove_physics_module();
 
-    sequence = stub->sequence;
+    m_sequence = stub->sequence;
 
 	create_draw_module();
 
 	draw_module()->set_texture(Picture_Manager::get_picture(stub->font_texture));
-	text_tex_coords.first = stub->tcoords;
-	text_tex_coords.second = stub->tcoords_count;
+    m_text_tex_coords.first = stub->tcoords;
+    m_text_tex_coords.second = stub->tcoords_count;
 
-	L_ASSERT(!(text_tex_coords.second < sequence.size()* tcpc));
-	for (unsigned int i = 0; i < sequence.size(); ++i)
+    L_ASSERT(!(m_text_tex_coords.second < m_sequence.size()* tcpc));
+    for (unsigned int i = 0; i < m_sequence.size(); ++i)
 	{
-		unsigned char current_char = sequence[i];
-		L_ASSERT(!(sequence_map.find(current_char) != sequence_map.end()));
-		sequence_map.emplace(current_char, &(text_tex_coords.first[tcpc * i]));
+        unsigned char current_char = m_sequence[i];
+        L_ASSERT(!(m_sequence_map.find(current_char) != m_sequence_map.end()));
+        m_sequence_map.emplace(current_char, &(m_text_tex_coords.first[tcpc * i]));
 	}
 
 	set_pos(stub->position);
-	width = stub->width;
-	height = stub->height;
+    m_width = stub->width;
+    m_height = stub->height;
 }
 
 
 
 void Text_Field::set_text(const std::string& _text)
 {
-    text = _text;
+    m_text = _text;
 
-    if (text.size() == 0)
+    if (m_text.size() == 0)
         return;
 
-	float* temp_coords = new float[text.size() * tcpc];
-	for (unsigned int i = 0; i < text.size(); ++i)
+    float* temp_coords = new float[m_text.size() * tcpc];
+    for (unsigned int i = 0; i < m_text.size(); ++i)
 	{
-		const float* ptr = sequence_map.at(text[i]);
+        const float* ptr = m_sequence_map.at(m_text[i]);
 		L_ASSERT(!(ptr == nullptr));
 
 		for (unsigned int j = 0; j < tcpc; ++j)
 			temp_coords[i * tcpc + j] = ptr[j];
 	}
-    m_draw_module->set_texture_coords(temp_coords, text.size() * tcpc);
+    m_draw_module->set_texture_coords(temp_coords, m_text.size() * tcpc);
 	delete[] temp_coords;
 
-	actual_symbol_width = (height * text.size() > width ? width / text.size() : height);
-	temp_coords = new float[text.size() * cpc];
-	for (unsigned int i = 0; i < text.size(); ++i)
+    m_actual_symbol_width = (m_height * m_text.size() > m_width ? m_width / m_text.size() : m_height);
+    temp_coords = new float[m_text.size() * cpc];
+
+    float h_alignment_stride = 0.0f;
+    if(m_horizontal_alignment == Horizontal_Alignment::right)
+        h_alignment_stride = m_actual_symbol_width * m_text.size();
+    else if(m_horizontal_alignment == Horizontal_Alignment::center)
+        h_alignment_stride = m_actual_symbol_width * m_text.size() * 0.5f;
+
+    float v_alignment_stride = 0.0f;
+    if(m_vertical_alignment == Vertical_Alignment::top)
+        v_alignment_stride = m_height;
+    else if(m_vertical_alignment == Vertical_Alignment::center)
+        v_alignment_stride = m_height * 0.5f;
+
+    for (unsigned int i = 0; i < m_text.size(); ++i)
 	{
 		std::pair<float, float> vertices[4];
-		vertices[0].first = actual_symbol_width * (float)i;							vertices[0].second = height;
-		vertices[1].first = actual_symbol_width * (float)i;							vertices[1].second = 0.0f;
-		vertices[2].first = actual_symbol_width * (float)i + actual_symbol_width;	vertices[2].second = 0.0f;
-		vertices[3].first = actual_symbol_width * (float)i + actual_symbol_width;	vertices[3].second = height;
+        vertices[0].first = m_actual_symbol_width * (float)i - h_alignment_stride;							vertices[0].second = m_height - v_alignment_stride;
+        vertices[1].first = m_actual_symbol_width * (float)i - h_alignment_stride;							vertices[1].second = 0.0f - v_alignment_stride;
+        vertices[2].first = m_actual_symbol_width * (float)i + m_actual_symbol_width - h_alignment_stride;	vertices[2].second = 0.0f - v_alignment_stride;
+        vertices[3].first = m_actual_symbol_width * (float)i + m_actual_symbol_width - h_alignment_stride;	vertices[3].second = m_height - v_alignment_stride;
 
 		temp_coords[0 + cpc * i] = vertices[0].first; temp_coords[1 + cpc * i] = vertices[0].second;
 		temp_coords[3 + cpc * i] = vertices[1].first; temp_coords[4 + cpc * i] = vertices[1].second;
@@ -93,20 +106,15 @@ void Text_Field::set_text(const std::string& _text)
 		temp_coords[12+ cpc * i] = vertices[2].first; temp_coords[13+ cpc * i] = vertices[2].second;
 		temp_coords[15+ cpc * i] = vertices[3].first; temp_coords[16+ cpc * i] = vertices[3].second;
 	}
-	for (unsigned int i = 2; i < text.size() * cpc; i += 3)
+    for (unsigned int i = 2; i < m_text.size() * cpc; i += 3)
 		temp_coords[i] = 0.0f;
-    m_draw_module->init_vertices(temp_coords, text.size() * cpc);
+    m_draw_module->init_vertices(temp_coords, m_text.size() * cpc);
     delete[] temp_coords;
 
-    unsigned int colors_count = text.size() * colorpc;
+    unsigned int colors_count = m_text.size() * colorpc;
     float* colors = new float[colors_count];
     for(unsigned int i=0; i<colors_count; ++i)
         colors[i] = 1.0f;
     m_draw_module->init_colors(colors, colors_count);
     delete[] colors;
-}
-
-const std::string& Text_Field::get_text() const
-{
-	return text;
 }
