b306c16bf903f62456775a4fe7a60954b86a5692
Артём <sh0n556@bk.ru>
2024-08-19 02:36:55 +0300

    added hard cap for fps timer

--- source/FPS_Timer.cpp
+++ source/FPS_Timer.cpp
@@ -5,7 +5,7 @@ using namespace LEti;
 
 void FPS_Timer::set_target_fps(unsigned int _value)
 {
-    m_target_fps = 60;
+    m_target_fps = _value;
     m_frame_duration = std::chrono::duration<float>(1.0f / m_target_fps);
 }
 
@@ -18,12 +18,18 @@ void FPS_Timer::tick()
     previous_time = current_time;
     accumulated_time += elapsed_time;
 
-    while (accumulated_time >= m_frame_duration)
+    unsigned int updates_amount = 0;
+
+    while (accumulated_time >= m_frame_duration && updates_amount < 5)
     {
         m_on_tick(m_frame_duration.count());
 
         accumulated_time -= m_frame_duration;
+        ++updates_amount;
     }
+
+    while (accumulated_time >= m_frame_duration)
+        accumulated_time -= m_frame_duration;
 
     std::chrono::duration<float> sleep_time = m_frame_duration - accumulated_time;
     if (sleep_time > std::chrono::duration<float>(0.0f))
