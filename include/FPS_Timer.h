b2ac5f69c8360817aac9ccde181293db0d2b6975
Артём <sh0n556@bk.ru>
2025-03-12 19:27:11 +0300

    added some logging

--- include/FPS_Timer.h
+++ include/FPS_Timer.h
@@ -2,6 +2,7 @@
 
 #include <thread>
 #include <chrono>
+#include <string>
 
 #include <Stuff/Function_Wrapper.h>
 
@@ -15,14 +16,23 @@ namespace LEti
         unsigned int m_target_fps = 60;
         std::chrono::duration<float> m_frame_duration{1.0f / m_target_fps};
 
-        std::chrono::high_resolution_clock::time_point previous_time = std::chrono::high_resolution_clock::now();
+        std::chrono::high_resolution_clock::time_point m_previous_time = std::chrono::high_resolution_clock::now();
+
+        float m_duration_counter = 0.0f;
+        unsigned int m_frames_counter = 0;
 
         LST::Function<void(float _dt)> m_on_tick;
+
+    public:
+        inline static const std::string& low_fps_log_level() { static std::string name = "LOW_FPS"; return name; }
 
     public:
         inline void set_on_tick(const LST::Function<void(float _dt)>& _on_tick) { m_on_tick = _on_tick; }
 
         void set_target_fps(unsigned int _value);
+
+    private:
+        void M_update_frames_counter(float _dt);
 
     public:
         void tick();
--- source/FPS_Timer.cpp
+++ source/FPS_Timer.cpp
@@ -1,5 +1,7 @@
 #include <FPS_Timer.h>
 
+#include <L_Debug/L_Debug.h>
+
 using namespace LEti;
 
 
@@ -11,17 +13,38 @@ void FPS_Timer::set_target_fps(unsigned int _value)
 
 
 
+void FPS_Timer::M_update_frames_counter(float _dt)
+{
+    ++m_frames_counter;
+    m_duration_counter += _dt;
+    if(m_duration_counter < 1.0f)
+        return;
+
+    L_DEBUG_FUNC_NOARG([this]()
+    {
+        if(m_frames_counter < m_target_fps)
+            L_LOG(low_fps_log_level(), "FPS: " + std::to_string(m_frames_counter));
+    });
+
+    m_duration_counter = 0.0f;
+    m_frames_counter = 0;
+}
+
+
+
 void FPS_Timer::tick()
 {
     std::chrono::high_resolution_clock::time_point current_time = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<float> elapsed_time = current_time - previous_time;
-    previous_time = current_time;
+    std::chrono::duration<float> elapsed_time = current_time - m_previous_time;
+    m_previous_time = current_time;
 
     float dt = elapsed_time.count();
     if(dt > m_frame_duration.count() * 5.0f)
         dt = m_frame_duration.count() * 5.0f;
 
     m_on_tick(dt);
+
+    M_update_frames_counter(dt);
 
     std::chrono::duration<float> sleep_time = m_frame_duration - elapsed_time;
     if (sleep_time > std::chrono::duration<float>(0.0f))
